eba8d4d1a41874d31a9b469bf3b03afe81124eec
Ahmadov Ibrahim <ibrahimakh47@gmail.com>
2023-07-20 23:28:51 +0400

    First commit

---
+++ src/main/java/com/ai/openaiembedding/OpenAIEmbeddingApplication.java
@@ -0,0 +1,14 @@
+package com.ai.openaiembedding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAIEmbeddingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAIEmbeddingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ai/openaiembedding/OpenAIEmbeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.openaiembedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAIEmbeddingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
